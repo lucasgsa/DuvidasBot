1875e339b8ae1a235643e8a8f24b4c73f572d65d
Lucas Gabriel KPNz <48533541+lucasgsa@users.noreply.github.com>
2021-03-30 14:10:03 -0300

    Substituindo o token, para um genérico

--- src/main/java/com/p1ufcg/util/Configs.java
+++ src/main/java/com/p1ufcg/util/Configs.java
@@ -1,7 +1,7 @@
 package com.p1ufcg.util;
 
 public class Configs {
-	 public static String discordToken = "ODI1MDU2MDI3NDY0NTY0NzY2.YF4XTQ.Z4Y-CZ7XR8L9i4WPQf4vu62HVIw";
+	 public static String discordToken = "TOKEN AQUI";
 	 public static String duvidaCommand = "!duvida";
 	 public static String duvidaHelpReaction = "✋";
 	 public static String duvidaRespondida = "✅";
